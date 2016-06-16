9d7e5f8526e8208548acbd648646bae057c3831b
unfoundbug <github@theadmin.co.uk>
2016-06-16 10:13:33 +0100

    Add initial files to repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace iteadTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Selected Directory";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Copper Layer - Top",
+            "Copper Layer - Bottom",
+            "Solder Mask - Top",
+            "Solder Mask - Bottom",
+            "Silkscreen - Top",
+            "Silkscreen - Bottom",
+            "Drill Points",
+            "Outline"});
+            this.checkedListBox1.Location = new System.Drawing.Point(53, 88);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(146, 124);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Check Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(53, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Prepare for manufacture";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 309);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.Win32;
+namespace iteadTool
+{
+    public partial class Form1 : Form
+    {
+        enum eFileType
+        {
+            eCopTop,
+            eCopBot,
+            eSolTop,
+            eSolBot,
+            eSilTop,
+            eSilBot,
+            eDrill,
+            eOutline
+        }
+        public void loadDir()
+        {
+            RegistryKey reg_key =
+            Registry.CurrentUser.OpenSubKey("Software", true);
+            RegistryKey sub_key = reg_key.CreateSubKey("IteadTool");
+            textBox1.Text = (string) sub_key.GetValue("CurrentDirectory", Directory.GetCurrentDirectory());
+        }
+        public void saveDir()
+        {
+            RegistryKey reg_key =
+            Registry.CurrentUser.OpenSubKey("Software", true);
+            RegistryKey sub_key = reg_key.CreateSubKey("IteadTool");
+            sub_key.SetValue("CurrentDirectory", textBox1.Text);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            loadDir();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd.SelectedPath = textBox1.Text;
+            fbd.ShowDialog();
+            textBox1.Text = fbd.SelectedPath;
+        }
+        
+        string m_strProjectName;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveDir();
+            string[] strFiles;
+            try
+            {
+
+                strFiles = Directory.GetFiles(textBox1.Text, "*.*");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Directory open failed");
+                return;
+            }
+            var strWantedFiles = strFiles.Where(s => s.EndsWith(".gbr") || s.EndsWith(".drl"));
+            checkedListBox1.Enabled = false;
+            foreach( int i in checkedListBox1.CheckedIndices)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+            
+            string strNameFetch = Path.GetFileName(strWantedFiles.First());
+            string strProjectName = strNameFetch.Substring(0, strNameFetch.LastIndexOf(" - "));
+
+            m_strProjectName = strProjectName;
+            button3.Enabled = true;
+            button3.Text = "Prepare " + m_strProjectName;
+            foreach (string strFileName in strWantedFiles)
+            {
+                if (strFileName.Contains("Bottom Copper.gbr"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eCopBot, true);
+                }
+                if (strFileName.Contains("Top Copper.gbr"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eCopTop, true);
+                }
+                if (strFileName.Contains("Bottom Copper (Resist).gbr"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eSolBot, true);
+                }
+                if (strFileName.Contains("Top Copper (Resist).gbr"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eSolTop, true);
+                }
+                if (strFileName.Contains("Top Silkscreen.gbr"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eSilTop, true);
+                }
+                if (strFileName.Contains("Bottom Silkscreen.gbr"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eSilBot, true);
+                }
+                if (strFileName.Contains("[Through Hole].drl"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eDrill, true);
+                }
+                if (strFileName.Contains("Board Outline"))
+                {
+                    checkedListBox1.SetItemChecked((int)eFileType.eOutline, true);
+                }
+            }
+            checkedListBox1.Enabled = true;
+        }
+        bool CopyAttempt(string strOld, string strNew)
+        {
+            try
+            {
+                File.Copy(strOld, strNew);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strNewDirectory = Path.Combine(textBox1.Text, m_strProjectName);
+            string strOldDirectory = textBox1.Text;
+            try
+            { Directory.Delete(strNewDirectory, true); }
+            catch (Exception ex) { };
+            Directory.CreateDirectory(strNewDirectory);
+
+            string strResult = "";
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Top Copper.gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GTL");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "ERROR: NO TOP COPPER\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Bottom Copper.gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GBL");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "ERROR: NO BOTTOM COPPER\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Top Copper (Resist).gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GTS");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "ERROR: No top resist\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Bottom Copper (Resist).gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GBS");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "ERROR: No bottom resist\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Top Silkscreen.gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GTO");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "WARNING: No top silkscreen\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Bottom Silkscreen.gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GBO");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "WARNING: No bottom silkscreen\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Drill Data - [Through Hole].drl");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".DRL");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "ERROR: NO DRILL DATA\n\r";
+            }
+            {
+                string strOldPath = Path.Combine(strOldDirectory, m_strProjectName + " - Board Outline.gbr");
+                string strNewPath = Path.Combine(strNewDirectory, m_strProjectName + ".GKO");
+                if (!CopyAttempt(strOldPath, strNewPath))
+                    strResult += "WARNING: No outline file\n\r";
+            }
+            string strZipName = Path.Combine(strOldDirectory, m_strProjectName + ".zip");
+            File.Delete(strZipName);
+            if (strResult.Contains("ERROR"))
+                strResult += "COMPRESSION SKIPPED DUE TO ERROR";
+            else
+            {
+                try
+                {
+                    ZipFile.CreateFromDirectory(strNewDirectory, strZipName, CompressionLevel.Optimal, false);
+                }
+                catch (Exception ex)
+                {
+                    strResult += "Zip error: " + ex.ToString();
+                }
+
+            }
+            MessageBox.Show("Attempt complete results below:\n\r" + strResult);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            checkedListBox1.Enabled = false;
+
+            foreach (int i in checkedListBox1.CheckedIndices)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+            checkedListBox1.Enabled = true;
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if(checkedListBox1.Enabled)
+                e.NewValue = e.CurrentValue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
